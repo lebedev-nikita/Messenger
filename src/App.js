22bcb11d8977fbb3fce5978e5ba3d4f478d9cc77
Nikita Lebedev <y87cgp@gmail.com>
2019-10-18 00:16:47 +0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1> Hello,World!!! </h1>///
+  );
+}
+
+function AppRender() {
+    return(    
+        <div className="App">
+          <App />
+        </div>///      
+    );
+}
+
+export default AppRender;
