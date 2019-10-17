b94f425a3db8f70e44c516b6a011a110b8f1d888
max <mx181199@gmail.com>
2019-10-18 01:30:14 +0300

    Change everything

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 // import './index.css';
-import AppRender from './App';
+import AppRender from './js/App';
 
 
 ReactDOM.render(<AppRender />, document.getElementById('root'));
---
+++ src/js/InputField.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './css/InputField.css'
+
+class InputField extends React.Component
+{
+	
+    render(){
+    	return(
+    		<div className="InputField"> 
+    		</div>///
+    	);
+    }
+}
+
+export default InputField;
