58493729b34784e5d35bcb88e88e75696955b70f
max <mx181199@gmail.com>
2019-10-18 22:40:59 +0300

    first mesage

--- src/js/InputField.js
+++ src/js/InputField.js
@@ -3,12 +3,41 @@ import './css/InputField.css'
 
 class InputField extends React.Component
 {
-	
+	constructor(props) {
+    super(props);
+    this.state = { text_mes : ''};
+  	this.handleInput = this.handleInput.bind(this);
+  	this.handlePress = this.handlePress.bind(this);
+  	}
+    
+
+  	handleInput(event){
+  		  this.setState({text_mes : event.target.value})
+  	}
+
+  	handlePress(event){
+  		if (event.key == "Enter"){
+  		//this.setState({text : "" });
+  	    }
+  	}
+
     render(){
+    	const tt=this.state.text_mes;
     	return(
-    		<div className="InputField"> 
-    		</div>///
-    	);
+    		<header>
+          <div> 
+    			    <textarea type="text" value={this.state.text} onChange={this.handleInput}
+                  wrap="soft"
+                  className="InputField"
+              />
+    		  </div>
+            <textarea type="text" value={tt}
+                  wrap="soft"
+                  className="text"
+                  readonly="readonly"
+              />
+    	  </header>///
+      );
     }
 }
 
