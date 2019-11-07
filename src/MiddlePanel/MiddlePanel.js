c3d67a47030a5c6ef008d30f47648939e4b24847
Nikita Lebedev <y87cgp@gmail.com>
2019-11-07 19:30:31 +0300

    Пока нормально

--- src/MiddlePanel/MiddlePanel.js
+++ src/MiddlePanel/MiddlePanel.js
@@ -5,8 +5,35 @@ class MiddlePanel extends React.Component
 {
     render(){
         return (
-            <div className="LeftPanel">
+            <div className="MiddlePanel">
+                <div className="MiddlePanelHeader">
+                </div>
+                <div className="MiddlePanelChat">
+                    <Message />
+                    <Message />
+                    <Message />
+                    <Message />
+                    <Message />
+                    <Message />
+                </div>
+                <div className="MiddlePanelBottom">
+                </div>
             </div>
         );
     }   
 }
+
+class Message extends React.Component
+{
+
+    render(){
+        return(
+            <div className="Message">
+                <div className="MessageHeader"> </div>
+                <div> hello </div>
+            </div>
+        );
+    }
+}
+
+export default MiddlePanel;
