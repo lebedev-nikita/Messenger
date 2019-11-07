a619143558ba81379b052493e749852f80db4402
Nikita Lebedev <y87cgp@gmail.com>
2019-11-07 19:29:07 +0300

    Пока нормально

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import LeftPanel from './LeftPanel/LeftPanel.js'
+import MiddlePanel from './MiddlePanel/MiddlePanel.js'
 function App() {
   return (
     <LeftPanel />
---
+++ src/MiddlePanel/MiddlePanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MiddlePanel.css';
+
+class MiddlePanel extends React.Component
+{
+    render(){
+        return (
+            <div className="LeftPanel">
+            </div>
+        );
+    }   
+}
